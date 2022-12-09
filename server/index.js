acb8e84f55f85387e8fd34c036c6685fc66fc38d
MightySpaceman <spacemanmcspace40@gmail.com>
2022-12-09 17:12:31 +1100

    login added!

--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@ const { db } = require("./database");
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
+app.use('/style.css', express.static('webapp/style.css'));
+app.use('/webapp.js', express.static('webapp/webapp.js'));
 const config = require('./serverConfig.json');
 
 // middleware thing idk I found this on stackoverflow (and yeah I admit it)
@@ -12,11 +14,13 @@ app.get('/', (req, res) => {
   // redirect to the html index if the URL is accessed without a path. Access from the CLI and possibly the mobile app will have a specific URL they go to to get 
   // tailored data.
 
+
   if (config.usePassword == true) {
-    res.sendFile("webapp/login.html", { root: __dirname });
+    res.sendFile("./webapp/login.html", { root: __dirname });
   }
   else {
-    res.sendFile("webapp/index.html", { root: __dirname });
+    res.sendFile("./webapp/index.html", { root: __dirname });
+    app.use('/', express.static('webapp'));
   }
 });
 
@@ -30,8 +34,21 @@ app.get('/list', (req, res) => {
 });
 }); 
 
-app.get('/login', (req, res) => {
-  res.sendFile('webapp/login.html');
+
+app.post('/', (req, res) => {
+  try {
+    if (req.body.password == "helloworld" && req.body.username == "kerbal") {
+      console.log(`Succesful login from ${req.ip}`);
+      res.sendFile('./webapp/index.html', { root: __dirname });
+    }
+    else {
+      console.log(`Unsuccesful login from ${req.ip}`);
+      res.send("<h1>Access.denied</h1>");
+    }
+  }
+  catch (err) {
+    res.send("<h1>Error 500</h1><h2>Internal server error</h2>")
+  }
 });
 
 app.post('/new', (req, res) => {
@@ -41,18 +58,8 @@ app.post('/new', (req, res) => {
     }
     catch (err) {
       console.log(err);
-      res.redirect("500.html");
+      res.sendFile('./webapp/500.html', { root: __dirname });
     }
-});
-
-app.post('/login', (req, res) => {
-  if (req.body.password == config.password) {
-    console.log(`Succesful login from ${req.ip}`);
-    res.sendFile('webapp/index.html', { root: __dirname });
-  }
-  else {
-    res.send("<h1>Access.denied</h1>");
-  }
 });
 
  // Listen idk
---
+++ server/webapp/cookies.js
@@ -0,0 +1,12 @@
+function getCookie(cookieName) {
+    let cookie = {};
+    document.cookie.split(';').forEach(function(el) {
+      let [key,value] = el.split('=');
+      cookie[key.trim()] = value;
+    })
+    
+    return cookie[cookieName];
+  };
+
+
+module.exports = { getCookie };
